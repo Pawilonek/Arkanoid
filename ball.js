a5a56f914822c05e468ceb41329969d44a5e01ad
Paweł Kamiński <pawel13k@gmail.com>
2020-01-05 22:22:47 +0100

    Change ball logic from speed of two dimensions to speed and angle

--- ball.js
+++ ball.js
@@ -2,34 +2,88 @@ export class Ball {
     // Image of the ball
     sprite = null;
 
-    speedX = 10;
-    speedY = 20;
+    angle = 135;
+    speed = 30;
 
-    posX = 1;
-    posY = 1;
+    posX = 250;
+    posY = 250;
 
     constructor(sprite) {
         this.sprite = sprite;
     }
 
+    updatePosition() {
+        let direction = parseInt(this.angle / 90);
+        let ratio = (this.angle % 90) / 90;
+        let speedRatio = this.speed * ratio;
+
+        switch (direction) {
+            case 0:
+                this.posX += speedRatio;
+                this.posY -= this.speed - speedRatio;
+                break;
+            case 1:
+                this.posX += this.speed - speedRatio;
+                this.posY += speedRatio;
+                break;
+            case 2:
+                this.posX -= speedRatio;
+                this.posY += this.speed - speedRatio;
+                break;
+            case 3:
+                this.posX -= this.speed - speedRatio;
+                this.posY -= speedRatio;
+                break;
+            default:
+                console.warn(`Invalid direction angle: ${this.angle}`)
+        }
+    }
+
+    checkColisions(object) {
+        if (this.posX >= object.width) {
+            // Bounce on right side
+            this.angle = 360 - this.angle;
+
+            return true;
+        }
+
+        if (this.posX <= object.posX) {
+            // Bounce on left side
+            this.angle = 360 - this.angle;
+            
+            return true
+        }
+
+        if (this.posY >= object.height) {
+            // Onunce on bvottom
+            if (this.angle <= 180) {
+                this.angle = 180 - this.angle;
+            } else {
+                this.angle = 360 - (this.angle - 180);
+            }
+
+            return true
+        }
+
+        if (this.posY <= object.posX) {
+            // Onunce on top
+            if (this.angle <= 90) {
+                this.angle = 180 - this.angle;
+            } else {
+                this.angle = 180 + 360 - this.angle;
+            }
+
+            return true
+        }
+
+        return false;
+    }
+
     loop() {
-        if (this.posX + this.speedX >= 600) {
-            this.speedX *= -1;
-        }
-        if (this.posX + this.speedX <= 0) {
-            this.speedX *= -1;
-        }
+        this.updatePosition();
 
-        this.posX += this.speedX;
 
-        if (this.posY + this.speedY >= 800) {
-            this.speedY *= -1;
-        }
-        if (this.posY + this.speedY <= 0) {
-            this.speedY *= -1;
-        }
 
-        this.posY += this.speedY;
     }
 
     draw(ctx) {
---
+++ borders.js
@@ -0,0 +1,15 @@
+export class Borders {
+    posX = 0;
+    posY = 0;
+    width = 0;
+    height = 0;
+
+    constructor(posX, posY, width, height) {
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+    }
+
+    
+}
